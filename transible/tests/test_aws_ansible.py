ede7f7abd458e3a79dc53520d5add7c0748ef841
Sagi Shnaidman <sshnaidm@redhat.com>
2022-07-26 15:23:25 +0300

    Add AWS tests

--- transible/tests/test_aws_ansible.py
+++ transible/tests/test_aws_ansible.py
@@ -7,11 +7,59 @@ class TestAWSAnsible:
 
     @pytest.fixture(autouse=True)
     def setup_class(self):
-        with open("test_data/all_data.yml") as f:
+        with open("test_data/aws_data.yml") as f:
             self.data = yaml.safe_load(f)
         self.data['cloud'] = "test-cloud"
         self.a = AmazonAnsibleCalculation(self.data)
         yield
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_vpcs(self, force_optimize, vars_file):
+        self.a.create_vpcs(force_optimize=force_optimize, vars_file=vars_file)
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_subnets(self, force_optimize, vars_file):
+        self.a.create_subnets(force_optimize=force_optimize, vars_file=vars_file)
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_security_groups(self, force_optimize, vars_file):
+        self.a.create_security_groups(force_optimize=force_optimize, vars_file=vars_file)
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_routers(self, force_optimize, vars_file):
+        self.a.create_routers(force_optimize=force_optimize, vars_file=vars_file)
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_nat_gateways(self, force_optimize, vars_file):
+        self.a.create_nat_gateways(force_optimize=force_optimize, vars_file=vars_file)
+
+    @pytest.mark.parametrize(
+        "force_optimize,vars_file",
+        [(True, True), (True, False), (False, False)],
+        ids=["optimize_and_vars_file", "optimize_no_vars_file", "no_optimize"]
+    )
+    def test_create_route_tables(self, force_optimize, vars_file):
+        self.a.create_route_tables(force_optimize=force_optimize, vars_file=vars_file)
 
     @pytest.mark.parametrize(
         "force_optimize,vars_file",
